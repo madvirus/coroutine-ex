dec9de97f0432aa1a8e9e004f8d03aebff2ebb01
madvirus <madvirus@madvirus.net>
2018-11-13 06:54:47 +0900

    Initial commit

---
+++ src/main/kotlin/basic.kt
@@ -0,0 +1,17 @@
+package basic
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main(args: Array<String>) {
+    GlobalScope.launch { // launch new coroutine in background and continue
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,") // main thread continues here immediately
+    runBlocking {     // but this expression blocks the main thread
+        delay(2000L)  // ... while we delay for 2 seconds to keep JVM alive
+    }
+}
